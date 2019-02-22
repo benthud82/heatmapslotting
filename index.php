04329e6f84e7c39adb0257099a41ac656d8d3532
Bentley <'benthud82@gmail.com'>
2019-02-21 21:07:47 -0600

    nightly build

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-include 'googlekey/googlekey.php';
-
 echo 'Google should be tracking now!';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Title of the document</title>
+    </head>
+
+    <body>
+        Content of the document......
+    </body>
+<?php include 'googlekey/googlekey.php';  ?>
+</html>
